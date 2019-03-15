cbb2fdbde90e49908c858c301380da7978e0d639
Blake Smith <blakesmith0@gmail.com>
2019-03-15 00:16:10 -0500

    Reorder methods, add license to source file

--- src/main/java/com/sproutsocial/feign/GoogleHttpClient.java
+++ src/main/java/com/sproutsocial/feign/GoogleHttpClient.java
@@ -1,3 +1,28 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Sprout Social
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*SOFTWARE.
+*/
+
+
 package com.sproutsocial.feign;
 
 import com.google.api.client.http.ByteArrayContent;
@@ -37,29 +62,13 @@ public class GoogleHttpClient implements Client {
     public final Response execute(final Request inputRequest,
                                   final Request.Options options) throws IOException {
         final HttpRequest request = convertRequest(inputRequest, options);
-        HttpResponse response = request.execute();
+        final HttpResponse response = request.execute();
         return convertResponse(inputRequest, response);
-    }
-
-    private final Response convertResponse(final Request inputRequest,
-                                           final HttpResponse inputResponse) throws IOException {
-        Integer contentLength = null;
-        if (inputResponse.getHeaders().getContentLength() != null &&
-            inputResponse.getHeaders().getContentLength() <= Integer.MAX_VALUE) {
-            contentLength = inputResponse.getHeaders().getContentLength().intValue();
-        }
-        return Response.builder()
-            .body(inputResponse.getContent(), contentLength)
-            .status(inputResponse.getStatusCode())
-            .reason(inputResponse.getStatusMessage())
-            .headers(toMap(inputResponse.getHeaders()))
-            .request(inputRequest)
-            .build();
     }
 
     private final HttpRequest convertRequest(final Request inputRequest,
                                              final Request.Options options) throws IOException {
-        // Setup request body
+        // Setup the request body
         HttpContent content = null;
         if (inputRequest.requestBody().length() > 0) {
             final Collection<String> contentTypeValues = inputRequest.headers().get("Content-Type");
@@ -81,6 +90,7 @@ public class GoogleHttpClient implements Client {
         for (final Map.Entry<String, Collection<String>> header : inputRequest.headers().entrySet()) {
             headers.set(header.getKey(), header.getValue());
         }
+        // Some servers don't do well with no Accept header
         if (inputRequest.headers().get("Accept") == null) {
             headers.setAccept("*/*");
         }
@@ -92,6 +102,22 @@ public class GoogleHttpClient implements Client {
             .setFollowRedirects(options.isFollowRedirects())
             .setThrowExceptionOnExecuteError(false);
         return request;
+    }
+
+    private final Response convertResponse(final Request inputRequest,
+                                           final HttpResponse inputResponse) throws IOException {
+        final HttpHeaders headers = inputResponse.getHeaders();
+        Integer contentLength = null;
+        if (headers.getContentLength() != null && headers.getContentLength() <= Integer.MAX_VALUE) {
+            contentLength = inputResponse.getHeaders().getContentLength().intValue();
+        }
+        return Response.builder()
+            .body(inputResponse.getContent(), contentLength)
+            .status(inputResponse.getStatusCode())
+            .reason(inputResponse.getStatusMessage())
+            .headers(toMap(inputResponse.getHeaders()))
+            .request(inputRequest)
+            .build();
     }
 
     private final Map<String, Collection<String>> toMap(final HttpHeaders headers) {
